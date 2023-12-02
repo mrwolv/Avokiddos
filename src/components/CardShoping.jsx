e15a7ca0fffc391b29f12c03e65fc876210131ff
depu <87559657+mrwolv@users.noreply.github.com>
2023-12-02 22:23:08 +0530

    added icon in cardshopping

--- src/components/CardShoping.jsx
+++ src/components/CardShoping.jsx
@@ -1,20 +1,61 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { CiHeart } from 'react-icons/ci';
 
 const CardShoping = () => {
   return (
-    <article className='px-28 md:px-44 mt-8 md:mt-20'>
+    <article className="px-28 md:px-44 mt-8 md:mt-20">
       <Swiper spaceBetween={50} slidesPerView={4} loop={true}>
         <SwiperSlide>
-          <img src="/public/kid4.jpg" alt="kid4" />
+          <figure className="relative">
+            <img src="/public/kid4.jpg" alt="kid4" />
+          </figure>
+          <span className="md:absolute top-[28px] right-[24px] ">
+            <CiHeart className="text-[23px] font-semibold hover:text-red-400 hover:cursor-pointer" />
+          </span>
+          <div className=" md:absolute md:top-[50%] md:left-[14%] ">
+            <button className="border-none bg-[#EF564C] text-white px-12 py-2">
+              Add to cart
+            </button>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/public/kid4.jpg" alt="kid4" />
+          <figure className="relative">
+            <img src="/public/kid4.jpg" alt="kid4" />
+          </figure>
+          <span className="md:absolute top-[28px] right-[24px] ">
+            <CiHeart className="text-[23px] font-semibold hover:text-red-400 hover:cursor-pointer" />
+          </span>
+          <div className=" md:absolute md:top-[50%] md:left-[14%] ">
+            <button className="border-none bg-[#EF564C] text-white px-12 py-2">
+              Add to cart
+            </button>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/public/kid4.jpg" alt="kid4" />
+          <figure className="relative">
+            <img src="/public/kid4.jpg" alt="kid4" />
+          </figure>
+          <span className="md:absolute top-[28px] right-[24px] ">
+            <CiHeart className="text-[23px] font-semibold hover:text-red-400 hover:cursor-pointer" />
+          </span>
+          <div className=" md:absolute md:top-[50%] md:left-[14%] ">
+            <button className="border-none bg-[#EF564C] text-white px-12 py-2">
+              Add to cart
+            </button>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/public/kid4.jpg" alt="kid4" />
+          <figure className="relative">
+            <img src="/public/kid4.jpg" alt="kid4" />
+          </figure>
+          <span className="md:absolute top-[28px] right-[24px] ">
+            <CiHeart className="text-[23px] font-semibold hover:text-red-400 hover:cursor-pointer" />
+          </span>
+          <div className=" md:absolute md:top-[50%] md:left-[14%] ">
+            <button className="border-none bg-[#EF564C] text-white px-12 py-2">
+              Add to cart
+            </button>
+          </div>
         </SwiperSlide>
       </Swiper>
     </article>
