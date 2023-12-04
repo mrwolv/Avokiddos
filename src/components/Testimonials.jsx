b3216ce444f80aae96b6d9d88fe651c017449cf1
depu <87559657+mrwolv@users.noreply.github.com>
2023-12-04 22:14:48 +0530

    style : Added testimonial Component heading

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
     >
       <div className='absolute inset-0 bg-red-400 bg-opacity-50'></div>
       <div className='absolute inset-0 flex items-center justify-center'>
-        <p className='text-4xl font-bold'>Hello</p>
+        <h1 className='text-4xl font-bold'>Testimonials</h1>
       </div>
     </section>
   );
