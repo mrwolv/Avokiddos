4be5e7a57fde53dc8cf547d4f6a1cfd64add1a2e
depu <87559657+mrwolv@users.noreply.github.com>
2023-12-04 15:28:27 +0530

    fix the headline section

--- src/components/HeadlineSecton.jsx
+++ src/components/HeadlineSecton.jsx
@@ -1,14 +1,14 @@
 
 const HeadlineSecton = () => {
   return (
-    <section className='bg-[#fedacd] h-[350px] '>
+    <section className='bg-[#fedacd]  '>
       <div className='flex items-center justify-between '>
         <div className='hidden mt-8 md:flex '>
           <img src='/Vector-5.svg' alt='' />
         </div>
         <div className='flex flex-col items-center gap-2 p-4 mt-16'>
           <img src='/quote.svg' alt='Quote' />
-          <h2 className='ml-4 px-10 text-[2.1em] font-lovesunshine md:text-[1.9em] md:px-16 md:ml-24 text-[#0b315f]'>
+          <h2 className='ml-4 text-[1.55rem] md:text-[1.9rem] font-lovesunshine  md:px-16 md:ml-24 text-[#0b315f]'>
             {' '}
             Every product is designed to let your kids be their most comfortable
             selves
