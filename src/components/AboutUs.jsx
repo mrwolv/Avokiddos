71b8b94b1f3996c6a8e69a853996a87b791b9272
depu <87559657+mrwolv@users.noreply.github.com>
2023-12-04 15:25:29 +0530

    completing the about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,7 +2,7 @@ import { IoIosArrowForward } from 'react-icons/io';
 
 const AboutUs = () => {
   return (
-    <section className='text-center md:text-left md:mt-12 mt-6 bg-[#fedacd] h-[450px] md:h-[450px] md:p-8 p-3.5 md:flex  md:justify-around '>
+    <section className=' md:mt-12 mt-6 bg-[#fedacd] h-[450px] md:h-[450px] md:p-8 p-5 md:flex  md:justify-around '>
       <div className='flex-1 mt-6 md:mt-20'>
         <div className='relative flex items-center'>
           <img
@@ -16,27 +16,30 @@ const AboutUs = () => {
           </h1>
         </div>
       </div>
-
-      <div className='mt-24 md:mt-10  mr-[20px] flex-1'>
-        
-          <h2 className='font-lovesunshine text-[#0b315f] text-[2.4rem]'>
-            About us
-          </h2>
+      <div className='mt-24 md:mt-10 mr-[20px] flex-1 relative'>
+        <img
+          src='Vector-14.png'
+          alt='vector14'
+          className=' absolute top-0 right-0 transform  translate-x-[23%] translate-y-[40%] md:translate-x-[22%] md:-translate-y-[3%] w-32 md:w-56'
+        />
+        <h2 className='font-lovesunshine text-[#0b315f] text-[2.4rem]'>
+          About us
+        </h2>
         <p className='mt-4 text-[#6c7175] text-[.9rem] md:text-[1rem]'>
           Little ones outgrow their dresses faster than one can think forcing
           the parents to always be on the lookout. Avokiddos brings for you
-          stylish and comfortable options to suit your sweethearts . Browse our
-          catalog for the exclusive range of colours and designs.
+          stylish and comfortable options to suit your sweethearts. Browse our
+          catalog for the exclusive range of colors and designs.
           <p className='mt-2'>
             A quick scroll down the page will reveal all categories for kids
             fashion.
           </p>
         </p>
-
-        <p className='flex items-center mt-6 text-white justify-centera'>
-          <span>Read our Story</span>
-          <IoIosArrowForward />
+        <p className='flex items-center justify-start mt-6 text-white hover:cursor-pointer'>
+          <span className='hover:underline'>Read our Story</span>
+          <IoIosArrowForward className='font-bold text-right text-red-500 hover:text-black' />
         </p>
+        
       </div>
     </section>
   );
