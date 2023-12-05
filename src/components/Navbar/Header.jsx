f61c2556d88b1c01c60bb119ef243c44133a0efc
depu <87559657+mrwolv@users.noreply.github.com>
2023-12-05 15:51:59 +0530

    style : fix t the responsive design of navbar in mobile

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
           </div>
         </div>
         <div className='text-[.6rem] md:text-[13.6px]'>
-          <p>20% Off on all orders above Rs. 999. Use code: fest_20</p>
+          <p className='px-2'>20% Off on all orders above Rs. 999. Use code: fest_20</p>
         </div>
 
         <div className='flex items-center gap-3 mr-2 md:mr-4 '>
@@ -35,14 +35,7 @@ const Header = () => {
           <CiFacebook size={20} />
         </div>
       </header>
-      <div className='flex items-center justify-center sm:flex '>
-        <img
-          src='/logo.png'
-          alt='Logo Image'
-          width={140}
-          className='w-[140px] h-[120px] md:w-[120px] md:h-[140px]'
-        />
-      </div>
+      
       <Navbar />
       <ImageSlider/>
       <HeadlineSecton />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import { IoPersonOutline } from 'react-icons/io5';
 import { GiBasket } from 'react-icons/gi';
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, Menu, message, Space } from 'antd';
+import { AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
   const [isLabelHidden, setIsLabelHidden] = useState(false);
-  const [open, setOpen] = useState(false);
 
   const handleInputChange = e => {
     const inputValue = e.target.value;
@@ -20,43 +20,20 @@ const Navbar = () => {
   };
 
   const boysItems = [
-    {
-      label: 'All Boys',
-      key: '1'
-    },
-    {
-      label: 'PlaySuits and Playsets',
-      key: '2'
-    },
-    {
-      label: 'Shirts',
-      key: '3'
-    },
-    {
-      label: 'Winter Wear',
-      key: '4'
-    }
+    { label: 'All Boys', key: '1' },
+    { label: 'PlaySuits and Playsets', key: '2' },
+    { label: 'Shirts', key: '3' },
+    { label: 'Winter Wear', key: '4' }
   ];
 
   const girlsItems = [
-    {
-      label: 'All Girls',
-      key: '1'
-    },
-    {
-      label: 'PlaySuits and Playsets',
-      key: '2'
-    },
-    {
-      label: 'Dresses',
-      key: '3'
-    },
-    {
-      label: 'Winter Wear',
-      key: '4'
-    }
+    { label: 'All Girls', key: '1' },
+    { label: 'PlaySuits and Playsets', key: '2' },
+    { label: 'Dresses', key: '3' },
+    { label: 'Winter Wear', key: '4' }
   ];
-
+  
+/* boys menu to fetch the boys dropdown */
   const boysMenu = (
     <Menu
       onClick={onClick}
@@ -67,7 +44,7 @@ const Navbar = () => {
       ))}
     </Menu>
   );
-
+/* Girls menu to fetch the girls dropdown */
   const girlsMenu = (
     <Menu
       onClick={onClick}
@@ -79,97 +56,91 @@ const Navbar = () => {
     </Menu>
   );
 
-  const Drawer = () => (
-    <div className='relative z-20 h-full bg-red-500 w-[50%] top-0 left-0'>
-      <p onClick={prevState => setOpen(!prevState)}>close</p>
-      <ul>
-        <li>hello</li>
-      </ul>
-    </div>
-  );
-
   return (
     <>
-      <div className='bg-white'></div>
-      <div className='relative cursor-pointer md:hidden bottom-20'>
-        {/* Hamburger icon for small screens */}
-        <div
-          className='px-4 text-2xl'
-          onClick={prevState => setOpen(!prevState)}
-        >
-          {open ? <Drawer/> : 'icon'}
+      <navbar className='flex items-center justify-between gap-8 px-4 md:flex-col md:px-0'>
+        <div className='md:hidden'>
+          <AiOutlineMenu />
         </div>
-      </div>
-
-      <section className='relative flex items-center justify-between px-8 mt-4 overflow-hidden md:flex '>
-        <div className='hidden md:px-8 md:gap-20 md:items-center md:flex '>
-          <form>
-            <div className='relative flex items-center justify-center'>
-              <label
-                className={`absolute left-4 text-xs  ${
-                  isLabelHidden ? 'opacity-0' : 'opacity-100'
-                }`}
-              >
-                Search
-              </label>
-              <div className='flex items-center '>
-                <input
-                  type='search'
-                  id='search'
-                  className='outline-none border-none focus:border-none bg-[#fff3f2] py-2 pl-8 pr-8'
-                  onChange={handleInputChange}
-                />
-                <div className='mb-4 cursor-pointer bg-red'>
-                  <FaSearch className='absolute right-2 text-[#419e7d] hover:bg-[#fedacd]' />
+        <div className='ml-10'>
+          <img
+            src='/public/final-logo.png'
+            alt=''
+            className='w-[75px] md:w-[150px]'
+          />
+        </div>
+        <div className='flex items-center gap-[17rem]'>
+          <div className='hidden md:px-8 md:gap-20 md:items-center md:flex '>
+            <form>
+              <div className='relative flex items-center justify-center'>
+                <label
+                  className={`absolute left-4 text-xs  ${
+                    isLabelHidden ? 'opacity-0' : 'opacity-100'
+                  }`}
+                >
+                  Search
+                </label>
+                <div className='flex items-center '>
+                  <input
+                    type='search'
+                    id='search'
+                    className='outline-none border-none focus:border-none bg-[#fff3f2] py-2 pl-8 pr-8'
+                    onChange={handleInputChange}
+                  />
+                  <div className='mb-4 cursor-pointer bg-red'>
+                    <FaSearch className='absolute right-2 text-[#419e7d] hover:bg-[#fedacd]' />
+                  </div>
                 </div>
               </div>
+            </form>
+            <div className='md:justify-between md:items-center md:flex '>
+              <ul className='flex items-center gap-4 hover:cursor-pointer '>
+                <li className='hover:text-red-400'>Home</li>
+                <li className='hover:text-red-400'>New Arrivals</li>
+                <li className='hover:text-red-400'>Shop All</li>
+                <li className=' hover:text-red-400'>
+                  <Dropdown overlay={boysMenu}>
+                    <a onClick={e => e.preventDefault()}>
+                      <Space>
+                        Boys
+                        <DownOutlined className='text-xs' />
+                      </Space>
+                    </a>
+                  </Dropdown>
+                </li>
+
+                <li className='hover:text-red-400'>
+                  <Dropdown overlay={girlsMenu}>
+                    <a onClick={e => e.preventDefault()}>
+                      <Space>
+                        Girls
+                        <DownOutlined className='text-xs' />
+                      </Space>
+                    </a>
+                  </Dropdown>
+                </li>
+                <li className='hover:text-red-400'>About</li>
+                <li className='hover:text-red-400'>Contacts</li>
+              </ul>
             </div>
-          </form>
-          <navbar className='md:justify-between md:items-center md:flex '>
-            <ul className='flex items-center gap-4 hover:cursor-pointer '>
-              <li className='hover:text-red-400'>Home</li>
-              <li className='hover:text-red-400'>New Arrivals</li>
-              <li className='hover:text-red-400'>Shop All</li>
-              <li className=' hover:text-red-400'>
-                <Dropdown overlay={boysMenu}>
-                  <a onClick={e => e.preventDefault()}>
-                    <Space>
-                      Boys
-                      <DownOutlined className='text-xs' />
-                    </Space>
-                  </a>
-                </Dropdown>
+          </div>
+          <div className='md:flex md:items-center md:justify-center'>
+            <ul className='flex gap-4 md:justify-center md:items-center'>
+              <li className='cursor-pointer hover:text-red-400'>
+                <CiHeart size={20} />
               </li>
 
-              <li className='hover:text-red-400'>
-                <Dropdown overlay={girlsMenu}>
-                  <a onClick={e => e.preventDefault()}>
-                    <Space>
-                      Girls
-                      <DownOutlined className='text-xs' />
-                    </Space>
-                  </a>
-                </Dropdown>
+              <li className='cursor-pointer hover:text-red-400'>
+                <IoPersonOutline size={20} />
               </li>
-              <li className='hover:text-red-400'>About</li>
-              <li className='hover:text-red-400'>Contacts</li>
+
+              <li className='cursor-pointer hover:text-red-400'>
+                <GiBasket size={20} />
+              </li>
             </ul>
-          </navbar>
+          </div>
         </div>
-        <div className='relative md:flex md:items-center md:justify-center '>
-          <ul className='flex gap-4 md:justify-center md:items-center md:flex hover:cursor-pointer'>
-            <li className='hover:text-red-400'>
-              <CiHeart size={20} />
-            </li>
-            <li className='hover:text-red-400'>
-              <IoPersonOutline size={20} />
-            </li>
-            <li className='hover:text-red-400'>
-              <GiBasket size={20} />
-            </li>
-          </ul>
-        </div>
-      </section>
+      </navbar>
     </>
   );
 };
