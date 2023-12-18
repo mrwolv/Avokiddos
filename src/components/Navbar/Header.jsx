66151e38fea1360fcd7847afd3e049c404c25532
depu <87559657+mrwolv@users.noreply.github.com>
2023-12-18 20:33:44 +0530

    Change : Change in text size in header

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -4,7 +4,7 @@ import { FaPhone, FaInstagram } from 'react-icons/fa6'
 const Header = () => {
     return (
         <div>
-            <header className='bg-[#EF564C] h-11 text-white flex items-center justify-between md:text-[13.6px] md:flex md:justify-between md:px-8'>
+            <header className='bg-[#EF564C] h-11 text-[.8rem] text-white flex items-center justify-between md:text-[1rem] md:flex md:justify-between md:px-8'>
                 <div className='hidden gap-8 cursor-pointer sm:flex sm:items-center '>
                     <div className='sm:flex sm:items-center '>
                         <CiMail size={18} />
